15f977fddccdf4960a637fb6a1a13268ae15f128
Yan <65673794+0-Luminous@users.noreply.github.com>
2024-12-26 21:10:11 +0200

    wip

---
+++ TaskFl0w/View/TaskFl0wApp.swift
@@ -0,0 +1,19 @@
+//
+//  TaskFl0wApp.swift
+//  TaskFl0w
+//
+//  Created by Yan on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskFl0wApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ClockView()
+        }
+    }
+}
